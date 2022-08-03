083bcd51baee30bd6562b779f2b5db629ab880f9
nurd0tid <mnur29054@gmail.com>
2022-08-03 10:28:52 +0700

    layouting home app

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+  <div class="page-body">
+    <div class="container-fluid">
+      <div class="page-header">
+        <div class="row">
+          <div class="col-sm-6">
+            <h3>Sample Page</h3>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+              <li class="breadcrumb-item">Pages</li>
+              <li class="breadcrumb-item active">Sample Page</li>
+            </ol>
+          </div>
+          <div class="col-sm-6">
+            <!-- Bookmark Start-->
+            <div class="bookmark">
+              <ul>
+                <li><a href="javascript:void(0)" data-container="body" data-bs-toggle="popover" data-placement="top"
+                    title="" data-original-title="Tables"><i data-feather="inbox"></i></a></li>
+                <li><a href="javascript:void(0)" data-container="body" data-bs-toggle="popover" data-placement="top"
+                    title="" data-original-title="Chat"><i data-feather="message-square"></i></a></li>
+                <li><a href="javascript:void(0)" data-container="body" data-bs-toggle="popover" data-placement="top"
+                    title="" data-original-title="Icons"><i data-feather="command"></i></a></li>
+                <li><a href="javascript:void(0)" data-container="body" data-bs-toggle="popover" data-placement="top"
+                    title="" data-original-title="Learning"><i data-feather="layers"></i></a></li>
+                <li><a href="javascript:void(0)"><i class="bookmark-search" data-feather="star"></i></a>
+                  <form class="form-inline search-form">
+                    <div class="form-group form-control-search">
+                      <input type="text" placeholder="Search..">
+                    </div>
+                  </form>
+                </li>
+              </ul>
             </div>
+            <!-- Bookmark Ends-->
+          </div>
         </div>
+      </div>
     </div>
-</div>
+    <!-- Container-fluid starts-->
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="card">
+            <div class="card-header pb-0">
+              <h5>Sample Card</h5><span>lorem ipsum dolor sit amet, consectetur adipisicing elit</span>
+            </div>
+            <div class="card-body">
+              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum."</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Container-fluid Ends-->
+  </div>
 @endsection
