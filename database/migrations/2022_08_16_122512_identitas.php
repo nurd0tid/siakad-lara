b338618e0ccecb1c0fb84079479959ca355649aa
nurd0tid <mnur29054@gmail.com>
2022-08-16 19:34:33 +0700

    make field migration identitas

--- database/migrations/2022_08_16_122512_identitas.php
+++ database/migrations/2022_08_16_122512_identitas.php
@@ -13,7 +13,22 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('identitas', function (Blueprint $table) {
+            $table->id('id_identitas');
+            $table->string('nm_sekolah');
+            $table->string('npsn', 50);
+            $table->string('nss', 50);
+            $table->text('almt_sekolah');
+            $table->string('kd_pos', 7);
+            $table->string('no_telp', 13);
+            $table->string('kelurahan');
+            $table->string('kecamatan');
+            $table->string('kab_kota');
+            $table->string('provinsi');
+            $table->string('website');
+            $table->string('email');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +38,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('identitas');
     }
 };
