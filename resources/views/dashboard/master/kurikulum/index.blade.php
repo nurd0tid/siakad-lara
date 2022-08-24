1a2d551429a8ca998a6688f51a5c5ce72ecef990
nurd0tid <mnur29054@gmail.com>
2022-08-24 18:27:52 +0700

    commit

--- resources/views/dashboard/master/kurikulum/index.blade.php
+++ resources/views/dashboard/master/kurikulum/index.blade.php
@@ -1,3 +1,93 @@
 @extends('layouts.app')
 @section('content')
+  @pushOnce('css')
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/datatables.css') }}">
+  @endPushOnce
+  <div class="page-body">
+    <div class="container-fluid">
+      <div class="page-header">
+        <div class="row">
+          <div class="col-sm-6">
+            <h3>Kurikulum</h3>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="index.html">Applications</a></li>
+              <li class="breadcrumb-item">Data Master</li>
+              <li class="breadcrumb-item active">Kurikulum</li>
+            </ol>
+          </div>
+          <div class="col-sm-6">
+            <!-- Bookmark Start-->
+            <div class="bookmark">
+              <ul>
+                <li><a href="javascript:void(0)" data-container="body" data-bs-toggle="popover" data-placement="top"
+                    title="" data-original-title="Tables"><i data-feather="inbox"></i></a></li>
+                <li><a href="javascript:void(0)" data-container="body" data-bs-toggle="popover" data-placement="top"
+                    title="" data-original-title="Chat"><i data-feather="message-square"></i></a></li>
+                <li><a href="javascript:void(0)" data-container="body" data-bs-toggle="popover" data-placement="top"
+                    title="" data-original-title="Icons"><i data-feather="command"></i></a></li>
+                <li><a href="javascript:void(0)" data-container="body" data-bs-toggle="popover" data-placement="top"
+                    title="" data-original-title="Learning"><i data-feather="layers"></i></a></li>
+                <li><a href="javascript:void(0)"><i class="bookmark-search" data-feather="star"></i></a>
+                  <form class="form-inline search-form">
+                    <div class="form-group form-control-search">
+                      <input type="text" placeholder="Search..">
+                    </div>
+                  </form>
+                </li>
+              </ul>
+            </div>
+            <!-- Bookmark Ends-->
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="col-sm-12">
+        <div class="card">
+          <div class="card-header">
+            <a href="" class="btn btn-primary">Add Kurikulum</a>
+          </div>
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class="display table table-bordered" id="basic-1">
+                <thead>
+                  <tr style="text-align: center">
+                    <th style="width: 55px">No</th>
+                    <th>Nama Kurikulum</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {{-- @foreach ($data as $zoom)
+                    <tr style="text-align: center">
+                      <td>{{ $loop->iteration }}</td>
+                      <td>{{ $zoom['topic'] }}</td>
+                      <td>{{ $zoom['start_time'] }}</td>
+                      @if ($zoom['duration'] < 30)
+                        <td><span class="span badge rounded-pill pill-badge-secondary">{{ $zoom['duration'] }}</span></td>
+                      @else
+                        <td><span class="span badge rounded-pill pill-badge-primary">{{ $zoom['duration'] }}</span></td>
+                      @endif
+                      <td style="width: 200px"><a href="{{ $zoom['join_url'] }}"
+                          class="txt-primary">{{ $zoom['join_url'] }}</a></td>
+                      <td>
+                        <a href="http://" class="btn btn-primary btn-xs"><i class="fa fa-edit"></i></a>
+                        <a href="http://" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i></a>
+                      </td>
+                    </tr>
+                  @endforeach --}}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  @pushOnce('js')
+    <script src="{{ asset('assets/js/datatable/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/js/datatable/datatables/datatable.custom.js') }}"></script>
+    <script src="{{ asset('assets/js/tooltip-init.js') }}"></script>
+  @endPushOnce
 @endsection
